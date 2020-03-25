2e7a85e86c1b9a52c22658a726d26a03a0349f65
Douglas Hirsh <douglas@codeup.com>
2020-03-25 11:47:49 -0500

    Finished the third for loop exercise.

--- js/for_loops.js
+++ js/for_loops.js
@@ -7,3 +7,15 @@ function showMultiplicationTable(input) {
 }
 
 showMultiplicationTable(7);
+
+// This is how you get a random number between 50 and 100
+
+for (var i = 0; i < 10; i++) {
+    var randomNumber = Math.floor(Math.random() * 180) + 20;
+
+    if(randomNumber % 2 === 0) {
+        console.log(randomNumber + ' is even');
+    } else {
+        console.log(randomNumber + ' is odd');
+    }
+}
