6b549319148a8bf540940bf817827717efa067fb
Fernando Mendoza <fernandoza@gmail.com>
2020-03-25 16:42:10 -0500

    feat: intro to arrays manipulation push/unshift

---
+++ js/arrays-manipulation.js
@@ -0,0 +1,12 @@
+var names = ["Douglas", "Fer", "Sophie", "Vivian"];
+
+console.log(names);
+
+// goes to the beginning of the array
+names.unshift( prompt("Give me your name:") );
+
+console.log(names);
+
+names.push( prompt("Give me your name:") );
+
+console.log(names);
